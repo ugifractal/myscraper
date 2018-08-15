bfa9431de49cd0e9c3d68908028b23cadfec8349
sugiarto <ugidmtest@gmail.com>
2018-08-15 08:59:48 +0700

    record as csv file

--- main.go
+++ main.go
@@ -3,17 +3,34 @@ package main
 import (
   "fmt"
   "github.com/gocolly/colly"
+  "encoding/csv"
+  "os"
+  "log"
 )
 
 func main() {
+  titles := []string{"Text", "Href"}
+  record := []string{"",""}
+  outfile, err := os.Create("./resultsfile.csv")
+  if err != nil {
+    log.Fatal(err)
+  }
+  w := csv.NewWriter(outfile)
+  w.Write(titles)
+
   fmt.Println("Starting...")
   c := colly.NewCollector(
     colly.AllowedDomains("www.fixkick.com"),
   )
+
   c.OnHTML("a[href]", func(e *colly.HTMLElement){
     link := e.Attr("href")
     fmt.Printf("Link found: %q -> %s\n", e.Text, link)
     c.Visit(e.Request.AbsoluteURL(link))
+    record[0] = e.Text
+    record[1] = link
+    w.Write(record)
+    w.Flush()
   })
 
   c.OnRequest(func(r *colly.Request){
